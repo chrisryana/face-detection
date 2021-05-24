ad02aafed21316d6704a09cf64b8f36cc9b72e87
Chris Ryana <kissunchic@yandex.ru>
2021-05-24 21:24:50 +0300

    Конфиг разделен на части

---
+++ src/data/config.js
@@ -0,0 +1,17 @@
+export const EMOTIONS = {
+    surprised: 'surprised',
+    disgusted: 'disgusted',
+    fearful: 'fearful',
+    sad: 'sad',
+    angry: 'angry',
+    happy: 'happy',
+}
+
+export const EMOTIONS_RU = {
+    [EMOTIONS.surprised]: 'Удивление',
+    [EMOTIONS.disgusted]: 'Отвращение',
+    [EMOTIONS.fearful]: 'Страх',
+    [EMOTIONS.sad]: 'Грусть',
+    [EMOTIONS.angry]: 'Злость',
+    [EMOTIONS.happy]: 'Счастье',
+}
--- src/data/questions.js
+++ src/data/questions.js
@@ -1,20 +1,4 @@
-export const EMOTIONS = {
-    surprised: 'surprised',
-    disgusted: 'disgusted',
-    fearful: 'fearful',
-    sad: 'sad',
-    angry: 'angry',
-    happy: 'happy',
-}
-
-export const EMOTIONS_RU = {
-    [EMOTIONS.surprised]: 'Удивление',
-    [EMOTIONS.disgusted]: 'Отвращение',
-    [EMOTIONS.fearful]: 'Страх',
-    [EMOTIONS.sad]: 'Грусть',
-    [EMOTIONS.angry]: 'Злость',
-    [EMOTIONS.happy]: 'Счастье',
-}
+import { EMOTIONS } from './config';
 
 export const QUESTIONS = [
     {
