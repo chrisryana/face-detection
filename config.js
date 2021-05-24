71be6e9aa07649e21dc2cbd8e161c996b7b293b2
Chris Ryana <kissunchic@yandex.ru>
2021-05-24 20:42:10 +0300

    Добавлены конфиги

---
+++ config.js
@@ -0,0 +1,147 @@
+export const EMOTIONS = {
+    surprised: 'surprised',
+    disgusted: 'disgusted',
+    fearful: 'fearful',
+    sad: 'sad',
+    angry: 'angry',
+    happy: 'happy',
+}
+
+export const EMOTIONS_RU = {
+    [EMOTIONS.surprised]: 'Удивление',
+    [EMOTIONS.disgusted]: 'Отвращение',
+    [EMOTIONS.fearful]: 'Страх',
+    [EMOTIONS.sad]: 'Грусть',
+    [EMOTIONS.angry]: 'Злость',
+    [EMOTIONS.happy]: 'Счастье',
+}
+
+export const QUESTIONS = [
+    {
+        img: 'https://cs5.pikabu.ru/post_img/2015/10/09/4/1444367594_252211042.gif',
+        alt: 'Кадр из фильма Дежурный папа, когда персонаж Эдди Мерфи оглядывает туалет после ребенка',
+        variants: [
+            { emotion: EMOTIONS.angry, isCorrect: false },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.disgusted, isCorrect: true },
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+        ]
+    }, {
+        img: 'https://i.gifer.com/UpFQ.gif',
+        alt: 'Халк выходит из огня и кричит',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: 'https://lh3.googleusercontent.com/proxy/iEP9_cZdaexJJCCJmxdiufqjkVu09_VIV49ruzi5rGVEx1Q8bCaN5QONRG5lr6X4WoKvPI_aTqBYg7X4udc4QHqI7QbdHyyl49aQr7MdZIIKD595LU_xAZW0cYkqbUyc6lOvq5dfjQkFffhaclyCy3lcUV6nXvsHIE8MilHPn9sVx5dtK9PmHwelRjb_9Iag1Rz2lxAf4Q0rtwawFw',
+        alt: 'Снежное чудище из Холодного сердца нашло корону и примеряет',
+        variants: [
+            { emotion: EMOTIONS.happy, isCorrect: true },
+            { emotion: EMOTIONS.surprised, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }, {
+        img: '',
+        alt: '',
+        variants: [
+            { emotion: EMOTIONS.fearful, isCorrect: false },
+            { emotion: EMOTIONS.angry, isCorrect: true },
+            { emotion: EMOTIONS.sad, isCorrect: false },
+            { emotion: EMOTIONS.happy, isCorrect: false },
+        ]
+    }
+]
---
+++ game.js
@@ -0,0 +1,8 @@
+import {EMOTIONS, EMOTIONS_RU, QUESTIONS} from './config.js';
+
+const STATE = {
+    currentQuestionIndex: 0,
+    choosenAnswer: EMOTIONS.disgusted,
+    timer: 15,
+    points: 0,
+}
