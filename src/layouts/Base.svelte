012d8aa6e075b4789932f9916fb192357ecccd6d
Chris Ryana <kissunchic@yandex.ru>
2021-07-06 22:09:01 +0300

    Добавлен базовый лэйаут

---
+++ src/layouts/Base.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Card from '../components/Card.svelte';
+</script>
+
+<div class="row">
+    <div class="column">
+        <Card>
+            <slot/>
+        </Card>
+    </div>
+    <!-- <div class="column">
+			<section class="card timer">
+				<h2 class="visually-hidden">Осталось:</h2>
+				30 секунд
+			</section>
+			<section class="card rules">
+				<h2 class="rules__tagline">Как играть?</h2>
+			</section>
+		</div> -->
+</div>
+
+<style>
+    .row {
+        display: flex;
+    }
+
+    .column {
+        margin-right: 2.3%;
+        flex-grow: 1;
+    }
+
+    .column:last-child {
+        margin-right: 0;
+    }
+</style>
