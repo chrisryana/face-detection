4f1c7bee64fddc6718cc50828521ce7e5fca2c6a
Chris Ryana <kissunchic@yandex.ru>
2021-07-06 22:14:35 +0300

    Намечена структура проекта

--- src/App.svelte
+++ src/App.svelte
@@ -1,195 +1,47 @@
 <script>
-	let name = '<8@';
+	import Description from './components/Description.svelte';
+	import Question from './components/Question.svelte';
+	import Result from './components/Result.svelte';
+	import { QUESTIONS } from './data/questions';
+
+	let questionIndex = -1;
+	let rightAnswersCount = 0;
+
+	function setNext() {
+		questionIndex += 1;
+	}
+
+	function onRight() {
+		rightAnswersCount += 1;
+		setNext();
+	}
+
+	function reset() {
+		questionIndex = -1;
+	}
 </script>
 
 <h1 class="visually-hidden">Игра повтори эмоцию по картинке</h1>
 <main class="content">
-	<div class="row">
-		<div class="column">
-			<section class="card questions">
-				<h2 class="questions__tagline">Вопрос 1</h2>
-				<div class="questions__images">
-					<div class="questions__image-wrapper">
-						<img src="https://cs5.pikabu.ru/post_img/2015/10/09/4/1444367594_252211042.gif"
-							alt="Кадр из фильма Дежурный папа, когда персонаж Эдди Мерфи оглядывает туалет после ребенка"
-							class="questions__image" />
-					</div>
-					<div class="video-placeholder questions__image-wrapper"></div>
-					<!-- <video id="video" width="312" height="250" autoplay muted></video> -->
-				</div>
-				<ul class="questions__answers">
-					<li class="questions__answers-item answers-item">
-						<button class="answers-item__button">вариант 1</button>
-					</li>
-					<li class="questions__answers-item answers-item">
-						<button class="answers-item__button">вариант 2</button>
-					</li>
-					<li class="questions__answers-item answers-item">
-						<button class="answers-item__button">вариант 3</button>
-					</li>
-					<li class="questions__answers-item answers-item">
-						<button class="answers-item__button">вариант 4</button>
-					</li>
-				</ul>
-			</section>
-		</div>
-		<div class="column">
-			<section class="card timer">
-				<h2 class="visually-hidden">Осталось:</h2>
-				30 секунд
-			</section>
-			<section class="card rules">
-				<h2 class="rules__tagline">Как играть?</h2>
-			</section>
-		</div>
-	</div>
+	{#if questionIndex < 0}
+		<Description onStart={setNext}/>
+	{/if}
+
+	{#if questionIndex >= 0}
+		<Question questionIndex={questionIndex} onRight={onRight} onEndTime={setNext}/>
+	{/if}
+	
+	{#if questionIndex + 1 > QUESTIONS.length}
+		<Result onBack={reset}/>
+	{/if}
 </main>
 
-<!-- <div class="wrapper">
-	<div class="loader">Распознаю твое личико (ಠ_ಠ)...</div>
-	<video id="video" width="720" height="576" autoplay muted></video>
-</div>
-<script src="face-api.min.js"></script>
-<script src="main.js"></script> -->
-
 <style>
-	body {
-		margin: 0;
-		padding: 0;
-		font-family: Arial, sans-serif;
-		font-size: 1rem;
-	}
-
-	* {
-		box-sizing: border-box;
-	}
-
-	a {
-		text-decoration: none;
-	}
-
-	img {
-		max-width: 100%;
-		height: auto;
-	}
-
-	ul, li {
-		margin: 0;
-		padding: 0;
-		list-style: none;
-	}
-
-	.visually-hidden:not(:focus):not(:active),
-	input[type="checkbox"].visually-hidden,
-	input[type="radio"].visually-hidden {
-		position: absolute;
-		width: 1px;
-		height: 1px;
-		margin: -1px;
-		border: 0;
-		padding: 0;
-		white-space: nowrap;
-		clip-path: inset(100%);
-		clip: rect(0 0 0 0);
-		overflow: hidden;
-	}
-
-	.wrapper {
-		padding: 20px;
-		height: 100vh;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.error, .loader {
-		padding: 8px 20px;
-		text-align: center;
-		border-radius: 3px;
-	}
-
-	.error {
-		background-color: #ffe9e9;
-		color: #ff7171;
-		border: 1px solid #ff7171;
-	}
-
-	.loader {
-		opacity: 0.6;
-		position: absolute;
-		background-color: #e9ffeb;
-		color: #34dc51;
-		border: 1px solid #34dc51;
-	}
-
-	canvas {
-		position: absolute;
-	}
-
-	.video-placeholder {
-		display: inline-block;
-		width: 312px;
-		height: 250px;
-		background-color: black;
-	}
-
 	.content {
+		display: block;
 		margin: 0 auto;
 		max-width: 1200px;
 		min-width: 885px;
 		padding: 2.3%;
-	}
-
-	.row {
-		display: flex;
-	}
-
-	.column {
-		margin-right: 2.3%;
-		flex-grow: 1;
-	}
-
-	.column:last-child {
-		margin-right: 0;
-	}
-
-	.card {
-		padding: 20px;
-		border: 1px solid darkblue;
-		border-radius: 3px;
-		background-color: lightblue;
-		margin-bottom: 15px;
-	}
-
-	.questions__images {
-		display: flex;
-		margin-bottom: 16px;
-	}
-
-	.questions__image-wrapper {
-		flex-grow: 1;
-	}
-
-	.answers-item {
-		margin-bottom: 10px;
-	}
-
-	.answers-item:last-child {
-		margin-bottom: 0;
-	}
-
-	.answers-item__button {
-		font-size: 1.3rem;
-		display: block;
-		width: 100%;
-		padding: 6px 16px;
-		border: none;
-		border-radius: 3px;
-	}
-
-	.answers-item__button:hover {
-		cursor: pointer;
-		background-color: black;
-		color: white;
 	}
 </style>
---
+++ src/components/Video.svelte
@@ -0,0 +1,86 @@
+<!-- <div class="wrapper">
+	<div class="loader">Распознаю твое личико (ಠ_ಠ)...</div>
+	<video id="video" width="720" height="576" autoplay muted></video>
+</div>
+<script src="face-api.min.js"></script> -->
+
+
+
+<style>
+    .wrapper {
+		padding: 20px;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.error, .loader {
+		padding: 8px 20px;
+		text-align: center;
+		border-radius: 3px;
+	}
+
+	.error {
+		background-color: #ffe9e9;
+		color: #ff7171;
+		border: 1px solid #ff7171;
+	}
+
+	.loader {
+		opacity: 0.6;
+		position: absolute;
+		background-color: #e9ffeb;
+		color: #34dc51;
+		border: 1px solid #34dc51;
+	}
+</style>
+
+<!-- (async function() {
+        const video = document.getElementById('video');
+        const wrapper = document.querySelector('.wrapper');
+        const loader = document.querySelector('.loader');
+        const error = '<div class="error">Для отображения контента разрешите доступ к камере</div>';
+    
+        await faceapi.nets.tinyFaceDetector.loadFromUri('./weights');
+        await faceapi.nets.faceRecognitionNet.loadFromUri('./weights');
+        await faceapi.nets.faceExpressionNet.loadFromUri('./weights');
+        await faceapi.nets.ageGenderNet.loadFromUri('./weights');
+    
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: { width: 720, height: 576 },
+            });
+            video.srcObject = stream;
+        } catch(err) {
+            wrapper.innerHTML = error;
+        }
+    
+        video.addEventListener('play', () => {
+            const canvas = faceapi.createCanvasFromMedia(video);
+            const displaySize = { width: video.width, height: video.height };
+    
+            wrapper.append(canvas);
+            faceapi.matchDimensions(canvas, displaySize);
+    
+            setInterval(async () => {
+                const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+                    .withAgeAndGender()
+                    .withFaceExpressions();
+                const resizedDetections = faceapi.resizeResults(detections, displaySize);
+                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                faceapi.draw.drawDetections(canvas, resizedDetections);
+                faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+                resizedDetections.forEach(detection => {
+                    const box = detection.detection.box
+                    const drawBox = new faceapi.draw.DrawBox(box, { label: `${Math.round(detection.age)} year old ${detection.gender}` })
+                    drawBox.draw(canvas)
+                })
+    
+                if (loader.style.display !== 'none') {
+                    loader.style.display = 'none';
+                } 
+            }, 100);
+        })
+    
+    })() -->
